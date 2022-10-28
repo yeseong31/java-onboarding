ccab71522297ec152bc22fa34f1c997378b350dd
yeseong31 <yeseong31@naver.com>
2022-10-28 11:31:04 +0900

    feat: 중복 문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+        checkDuplicateChar(stack, cryptogram);
         return answer;
     }
+
+    public static void checkDuplicateChar(Stack<Character> stack, String target) {
+        for (char c : target.toCharArray()) {
+            if (checkStack(stack, c)) {
+                stack.push(c);
+                continue;
+            }
+            while (!checkStack(stack, c)) {
+                stack.pop();
+            }
+        }
+    }
+
+    public static boolean checkStack(Stack<Character> stack, char c) {
+        return stack.empty() || stack.peek() != c;
+    }
+
 }
