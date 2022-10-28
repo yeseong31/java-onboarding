df44ceb9bd7b928aa9d74767fb8e28971ddac772
yeseong31 <yeseong31@naver.com>
2022-10-28 10:55:47 +0900

    feat: 입력값 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,43 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (checkException(pobi, crong)) {
+            return EXCEPTION;
+        }
         return answer;
     }
+
+    private static boolean checkException(List<Integer> pobi, List<Integer> crong) {
+        if (validatePageListLength(pobi) || validatePageListLength(crong)) {
+            return true;
+        }
+        if (validatePageNumber(pobi) || validatePageNumber(crong)) {
+            return true;
+        }
+        if (validatePageRange(pobi) || validatePageRange(crong)) {
+            return true;
+        }
+        return validatePageDifference(pobi) && !validatePageDifference(crong);
+    }
+
+    private static boolean validatePageListLength(List<Integer> list) {
+        return list.size() != 2;
+    }
+
+    private static boolean validatePageNumber(List<Integer> list) {
+        return list.get(0) % 2 != 1 || list.get(1) % 2 != 0;
+    }
+
+    private static boolean validatePageRange(List<Integer> list) {
+        return list.get(0) <= 1 || list.get(1) >= 400;
+    }
+
+    private static boolean validatePageDifference(List<Integer> list) {
+        return list.get(1) - list.get(0) != 1;
+    }
+
 }
