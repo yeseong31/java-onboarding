ded81628fbd1c6fe483ef952fd6647d569dcfffc
yeseong31 <yeseong31@naver.com>
2022-10-28 15:36:14 +0900

    feat: 금액 변환 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] coins = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < coins.length; i++) {
+            answer.add(i, (money / coins[i]));
+            money %= coins[i];
+        }
         return answer;
     }
+
 }
