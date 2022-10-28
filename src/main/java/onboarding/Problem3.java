4a9abee95f2147a9222f0941f616429193e692cd
yeseong31 <yeseong31@naver.com>
2022-10-28 16:14:44 +0900

    feat: 박수친 횟수를 계산하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
+
+    private static int countClap(int num) {
+        int result = 0;
+        String target = String.valueOf(num);
+        for (int i = 0; i < target.length(); i++) {
+            result += validate369(target.charAt(i));
+        }
+        return result;
+    }
+
+    private static int validate369(char c) {
+        if (c == '3' || c == '6' || c == '9') {
+            return 1;
+        }
+        return 0;
+    }
+
 }
