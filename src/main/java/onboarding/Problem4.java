6f3772ef9e696e0c9ed5ceb4af843be6271f7cf8
yeseong31 <yeseong31@naver.com>
2022-10-28 15:26:41 +0900

    feat: 문자 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            answer.append(convertAlphabet(c));
+        }
+        return answer.toString();
     }
+
+    private static char convertAlphabet(char c) {
+        if (!isAlphabet(c)) {
+            return c;
+        }
+        return convertChar(c);
+    }
+
+    private static boolean isAlphabet(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+
+    private static char convertChar(char c) {
+        if (Character.isUpperCase(c)) {
+            return (char)('Z' - (c - 'A'));
+        }
+        return (char)('z' - (c - 'a'));
+    }
+
 }
