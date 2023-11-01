53740aacf29fed3e2b8b31a4bc3c8a825590a3ea
yeseong31 <yeseong31@naver.com>
2023-11-01 22:19:21 +0900

    feat(Problem3): 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        long count = IntStream.range(1, number + 1)
+                .mapToObj(Problem3::convertToCharacterArray)
+                .mapToInt(Problem3::clapHands)
+                .sum();
+        return Math.toIntExact(count);
+    }
+
+    private static char[] convertToCharacterArray(int n) {
+        return String.valueOf(n)
+                .toCharArray();
+    }
+
+    private static int clapHands(char[] characterNumbers) {
+        int result = 0;
+        for (Character c : characterNumbers) {
+            if (satisfyCondition(c)) {
+                ++result;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    private static boolean satisfyCondition(Character c) {
+        int n = Character.getNumericValue(c);
+        return n != 0 && n % 3 == 0;
     }
 }
